6fc2562e06ad3d7a5218ecc3071773d3a44d7a9d
Tetsuji ITAGAKI <t-itagaki@atware.co.jp>
2022-05-18 11:37:11 +0900

    create project

---
+++ src/main/java/com/example/crudapp/SpringBootCrudApplicationWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApplicationWithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudApplicationWithThymeleafApplication.class, args);
+    }
+
+}
